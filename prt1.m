b02a1855f056d05c68857af3c645bc31056019b0
Ar-Jay C. <acmacagba1@my.scccd.edu>
2025-12-08 07:12:24 +0000

    Part 1,3,4 update

--- prt1.m
+++ prt1.m
@@ -1,1 +1,53 @@
+% System of Equations - Finding the Equation of a Parabola
 
+% Ask the user for three (x,y) points
+x1 = input('Enter x1:\n ');
+y1 = input('Enter y1:\n ');
+x2 = input('Enter x2:\n ');
+y2 = input('Enter y2:\n ');
+x3 = input('Enter x3:\n ');
+y3 = input('Enter y3:\n ');
+
+% Make sure all x-values are different
+if x1 == x2 | x1 == x3 | x2 == x3
+    fprintf('Error: the x-values must all be different.\n');
+    return
+end
+
+% Set up the system A * coeff = y
+A = [x1^2  x1  1;
+     x2^2  x2  1;
+     x3^2  x3  1];
+
+y = [y1; y2; y3];
+
+% Solve for [a; b; c]
+coeff = A \ y;
+a = coeff(1);
+b = coeff(2);
+c = coeff(3);
+
+% Display the equation of the parabola
+fprintf('\nThe equation of the parabola is:\n');
+fprintf('y = %.2f x^2 + %.2f x + %.2f\n\n', a, b, c);
+
+% Create x values for plotting the parabola
+xPlot = -10:0.1:10;
+yPlot = a*xPlot.^2 + b*xPlot + c;
+
+% Plot the parabola
+plot(xPlot, yPlot, 'b-');
+hold on;
+
+% Plot the three points entered by the user
+xPoints = [x1 x2 x3];
+yPoints = [y1 y2 y3];
+plot(xPoints, yPoints, 'ro', 'MarkerFaceColor', 'r');
+
+% Add title, labels, and grid
+title('Parabola Through Three Points');
+xlabel('x');
+ylabel('y');
+grid on;
+
+hold off;
--- pt4.m
+++ pt4.m
@@ -1,1 +1,36 @@
+% Project 3.4
+% Ask for number of players
+numPlayers = input('How many players do you want to enter:\n ');
 
+% For loop for player name and stats
+for i = 1:numPlayers
+    fprintf('\nEnter the name of the baseball player: ');
+    name = input('', 's');
+
+    fprintf('Enter the name of the team they play on: ');
+    team = input('', 's');
+
+    hits = input('Enter their number of hits: ');
+    atBats = input('Enter their number of at bats: ');
+    walks = input('Enter their number of walks: ');
+
+    players(i) = player(name, team, hits, walks, atBats);
+end
+
+% Players with 0.300 avg or higher for batting
+fprintf('\nThe players with a batting average higher than 0.300 are:\n\n');
+
+for i = 1:numPlayers
+    if players(i).average > 0.300
+        players(i).displayStats();
+    end
+end
+
+% Players with 0.350 avg or higher for on base percentage
+fprintf('\nThe players with an on-base percentage higher than 0.350 are:\n\n');
+
+for i = 1:numPlayers
+    if players(i).onBase > 0.350
+        players(i).displayStats();
+    end
+end
